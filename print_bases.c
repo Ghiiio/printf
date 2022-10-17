878da409e012cd671a44f42a4920546b9f0126f9
Abdulrasheed Okhuosami <110449913+Qaadaffi@users.noreply.github.com>
2022-10-17 16:38:42 +0100

    Update print_bases.c

--- print_bases.c
+++ print_bases.c
@@ -4,7 +4,7 @@
  * print_hex - prints a number in hexadecimal base,
  * in lowercase
  * @l: va_list arguments from _printf
- * @f: pointer to the struct flags that determines
+ * @f: is a pointer to the struct flags that determines
  * if a flag is passed to _printf
  * Description: the function calls convert() which in turns converts the input
  * number into the correct base and returns it as a string
